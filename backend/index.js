96af6b68b1a523bf6eb182129d4e7609e5644b83
Theeraphat <theeraphat.tr@gmail.com>
2025-01-09 23:12:51 +0700

    Add error and unauthorize handlers

--- backend/index.js
+++ backend/index.js
@@ -28,6 +28,13 @@ const fetchProblems = async () => {
     }
 }
 
+const authorize = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+    next()
+}
+
 const app = express()
 
 app.use(session({
@@ -59,16 +66,17 @@ app.use('/auth', authRoute)
 //     console.log(request)
 //     return response.status(200).send("Hello World")
 // })
+
+app.use(authorize)
+
   
 app.post('/problems', async (request,response) => {
     try{
         if(!request.body.id){
-            return response.status(400).send({message: 'Enter Problem ID'})
-        }
-        if(!request.user.id){
-            return response.status(400).send({message: 'Unauthorized'})
+            return response.status(400).json({message: 'Enter Problem ID'})
         }
         const selectedProblem = problemLists.find((problem) => `${problem.contestId}${problem.index}` === request.body.id)
+        if(!selectedProblem)  return response.status(400).json({message: 'Problem not found'})
         // return response.status(201).send(selectedProblem)
         const newProblem = {
             id: request.body.id,
@@ -79,11 +87,11 @@ app.post('/problems', async (request,response) => {
             userId: request.user.id
         }
         const problem = await Problem.create(newProblem)
-        return response.status(201).send(problem)
+        return response.status(201).json(problem)
     }
     catch (err) {
         console.log(err.message)
-        response.status(500).send({message: err.message})
+        response.status(500).json({message: err.message})
     }
 })
 
@@ -95,7 +103,7 @@ app.get('/problems', async (request,response) => {
     }
     catch (err){
         console.log(err.message)
-        return response.status(500).send({message: err.message})
+        return response.status(500).json({message: err.message})
     }
 })
 
@@ -107,20 +115,20 @@ app.delete('/problems/:id', async (request,response) => {
         const userId = request.user.id
 
         if(!userId){
-            return response.status(404).send({message: 'Unauthorized'})
+            return response.status(404).json({message: 'Unauthorized'})
         }
 
         const result = await Problem.findOneAndDelete({_id: id, userId})
 
         if(!result){
-            return response.status(404).send({message: 'Problem not found'})
+            return response.status(404).json({message: 'Problem not found'})
         }
 
-        return response.status(200).send({message: 'Deleted successfully'})
+        return response.status(200).json({message: 'Deleted successfully'})
     }
     catch (err) {
         console.log(err.message)
-        return response.status(500).send({message: err.message})
+        return response.status(500).json({message: err.message})
     }
 })
 
@@ -131,11 +139,11 @@ app.put('/problems/:id', async (request,response) => {
         const result = await Problem.findOneAndUpdate({_id: id, userId}, request.body)
 
         if(!result) return response.status(404).send({message: 'Problem not found'})
-        return response.status(200).send({message: "updated"})
+        return response.status(200).json({message: "updated"})
     }
     catch (error){
         console.log(error.message)
-        return response.status(500).send({message: error.message})
+        return response.status(500).json({message: error.message})
     }
 })
 
@@ -149,12 +157,13 @@ app.get('/problems/random', async (request,response) => {
         let problemTags = request.query.tags ? request.query.tags.split(',') : [];
         const solvedProblems = await Problem.find({userId})
         const unsolvedProblems = problemLists.filter((problem) => (!solvedProblems.includes(problem) &&typeof problem.rating !== 'undefined' && problem.rating >= minRating && problem.rating <= maxRating && problemTags.every(tag => problem.tags.includes(tag)) ))
+        if(!unsolvedProblems.length) return response.status(404).json({message: 'Problem not found'})
         const randomProblem = unsolvedProblems[Math.floor(Math.random()*unsolvedProblems.length)]
         return response.status(200).json(randomProblem)
     }
     catch (err){
         console.log(err.message)
-        return response.status(500).send({message: err.message})
+        return response.status(500).json({message: err.message})
     }
 })
 
@@ -166,7 +175,7 @@ mongoose
             console.log(`App is listening to port: ${PORT}`);
         });
         fetchProblems()
-        cron.schedule('*/1 * * * *', fetchProblems)
+        cron.schedule('*/5 * * * *', fetchProblems)
     })
     .catch((error => {
         console.log(error);
